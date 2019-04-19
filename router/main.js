c64b21c53fbc904053a4bbb73d816ac603a68060
chenwanan@exchain.com <imeahu@163.com>
2019-04-19 16:48:55 +0800

    feat: commit

--- router/main.js
+++ router/main.js
@@ -1,4 +1,7 @@
-exports.get = (ctx) => {
+const Topic = require('../lib/core').Topic;
+const User = require('../lib/core').$User;
+
+exports.get = async (ctx) => {
     const userInfo = await User.getUserByName(ctx.cookies.get("username"));
     ctx.render('index', {userInfo})
 }
